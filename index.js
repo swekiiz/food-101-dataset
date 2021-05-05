1a6c41178e06dc1cb1e2a1ae89cdf9e0908a9c03
swekiiz <best_n.dol@hotmail.com>
2021-05-05 10:31:16 +0700

    api

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const tf = require("@tensorflow/tfjs-node");
 const express = require("express");
+const multer = require("multer");
+
 const app = express();
 
-const callback = () => {
-  app.post("/", (req, res) => {
-    //upload images
-  });
+const classData = require('./class');
 
-  app.listen(3000, () => {
-    console.log("Start server at port 3000.");
-  });
+const handleError = (err, res) => {
+  res.status(500).contentType("text/plain").end("Oops! Something went wrong!");
 };
+
+const upload = multer({
+  dest: "./upload",
+});
 
 tf.loadLayersModel("file://models/tfjs/model.json")
   .then((model) => {
-    callback();
-    const result = model.predict('some picture')
+    app.post("/ml", upload.single("file"), (req, res) => {
+      const tempPath = req.file.path;
+      const targetPath = path.join(__dirname, "./uploads/image.png");
+
+      if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+        fs.rename(tempPath, targetPath, (err) => {
+          if (err) return handleError(err, res);
+          let image = req.file;
+          let tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([-1,128,128,3]).toFloat().expandDims();
+          let prediction = amodel.predict(tensorImg).data();
+          res.status(200).contentType("text/plain").end(classData[prediction[0]]);
+        });
+      } else {
+        fs.unlink(tempPath, (err) => {
+          if (err) return handleError(err, res);
+          res
+            .status(403)
+            .contentType("text/plain")
+            .end("Only .png files are allowed!");
+        });
+      }
+    });
+
+    app.listen(3000, () => {
+      console.log("Start server at port 3000.");
+    });
   })
   .catch((err) => {
     console.log(err);
